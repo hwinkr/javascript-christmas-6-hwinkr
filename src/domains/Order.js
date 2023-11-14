620b026c7c9a5fd16026f234f47ac47d6e0f97a6
hwinkr <dnddl8280@naver.com>
2023-11-14 11:40:11 +0900

    feat(Order): 우테코 식당에서 주문을 표현하는 Order 도메인 구현
    - 주문한 메뉴의 전체 금액을 계산, 주문한 메뉴들에 대한 할인 금액을 계산, 주문 정보를 생성하는 책임을 가짐
    - 고객이 한 주문이 유효한지 검증하는 책임을 가짐
    - VisitDate 도메인으로 부터 할인 타입(평일 or 주말)을 받아서 주문한 메뉴들에 대한 할인 금액을 계산하는 도메인 로직 구현

---
+++ src/domains/Order.js
@@ -0,0 +1,57 @@
+import { SEPERATORS } from '../constants/christmas-event.js';
+import deepFreeze from '../utils/deepFreeze.js';
+import OrderValidator from '../validators/OrderValidator.js';
+import Menu from './Menu.js';
+
+export default class Order {
+  #menuList;
+
+  constructor(order) {
+    OrderValidator.validate(order);
+    this.#menuList = this.#generateMenuList(order);
+  }
+
+  #createOrderForm(order) {
+    const orderList = order.split(SEPERATORS.order).map((orderMenu) => orderMenu.trim());
+    const orderForm = orderList.reduce((accForm, orderMenu) => {
+      const [name, quantity] = orderMenu.split(SEPERATORS.menu).map((item) => item.trim());
+      return [...accForm, { name, quantity }];
+    }, []);
+
+    return deepFreeze(orderForm);
+  }
+
+  #generateMenuList(order) {
+    const orderForm = this.#createOrderForm(order);
+    const menuList = orderForm.map((menu) => {
+      const { name, quantity } = menu;
+      return new Menu(name, Number(quantity));
+    });
+    return menuList;
+  }
+
+  calculateTotalAmount() {
+    const totalPrice = this.#menuList.reduce(
+      (accPrice, menu) => accPrice + menu.calculateMenuAmount(),
+      0,
+    );
+    return totalPrice;
+  }
+
+  calculateTotalDiscountAmount(discountType) {
+    const totalDiscountAmount = this.#menuList.reduce(
+      (accDiscount, menu) => accDiscount + menu.calculateDiscountAmount(discountType),
+      0,
+    );
+    return totalDiscountAmount;
+  }
+
+  getInfo() {
+    const orderInfo = this.#menuList.reduce(
+      (accInfo, menu) => [...accInfo, menu.getMenuInfo()],
+      [],
+    );
+
+    return deepFreeze(orderInfo);
+  }
+}
