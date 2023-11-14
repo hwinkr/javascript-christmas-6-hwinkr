332db281f53efb90162fcff92c22ffaa6898e664
hwinkr <dnddl8280@naver.com>
2023-11-14 11:45:57 +0900

    feat(VisitDate): 우테코 식당 방문 날짜를 표현하는 VisitDate 도메인 구현
    - 식당 방문 날짜에 따라서 달라지는 할인을 결정하는 책임을 가짐
    - 식당 방문 날짜가 유효한지 판단하는 책임을 가짐
    - 특별 할인, 평일 or 주말 할인, 크리스마스 디데이 할인 금액을 계산하는 도메인 로직 구현

---
+++ src/domains/VisitDate.js
@@ -0,0 +1,73 @@
+import {
+  BENEFIT_TYPE,
+  DISCOUNT_AMOUNT,
+  ERROR_MESSAGES,
+  EVENT_RULES,
+  SPECIAL_DISCOUNT_DATE,
+  WEEKEND,
+} from '../constants/christmas-event.js';
+
+export default class VisitDate {
+  #date;
+
+  constructor(date) {
+    this.#validate(date);
+    this.#date = date;
+  }
+
+  #validateDateType(date) {
+    if (typeof date !== 'number' || Number.isNaN(date)) {
+      throw new Error(ERROR_MESSAGES.invalidDate);
+    }
+  }
+
+  #validateDateRange(date) {
+    if (date < EVENT_RULES.startDay || date > EVENT_RULES.endDay) {
+      throw new Error(ERROR_MESSAGES.invalidDate);
+    }
+  }
+
+  #validate(date) {
+    this.#validateDateType(date);
+    this.#validateDateRange(date);
+  }
+
+  #isChristmasDiscountApplicable() {
+    return this.#date <= EVENT_RULES.christmasThreshold;
+  }
+
+  #isSpecialDiscountApplicable() {
+    return SPECIAL_DISCOUNT_DATE.includes(this.#date);
+  }
+
+  #isWeekend(day) {
+    return day === WEEKEND.friday || day === WEEKEND.saturday;
+  }
+
+  getWeeklyDiscountType() {
+    const dateObject = new Date(`${EVENT_RULES.year}-${EVENT_RULES.month}-${this.#date}`);
+    const day = dateObject.getDay();
+
+    return this.#isWeekend(day) ? BENEFIT_TYPE.weekendDiscount : BENEFIT_TYPE.weekdayDiscount;
+  }
+
+  getChristmasDiscountAmount() {
+    if (!this.#isChristmasDiscountApplicable()) {
+      return DISCOUNT_AMOUNT.zero;
+    }
+
+    const discountAmount =
+      DISCOUNT_AMOUNT.initialChristmasDiscount +
+      DISCOUNT_AMOUNT.dailyIncreaseAmount * (this.#date - EVENT_RULES.startDay);
+
+    return discountAmount;
+  }
+
+  getSpecialDiscountAmount() {
+    if (!this.#isSpecialDiscountApplicable()) {
+      return DISCOUNT_AMOUNT.zero;
+    }
+
+    return DISCOUNT_AMOUNT.specialDiscount;
+  }
+}
