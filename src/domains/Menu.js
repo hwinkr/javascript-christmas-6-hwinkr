f7bf1ee9d0b5a9758bdc3a60122596e50e560cf0
hwinkr <dnddl8280@naver.com>
2023-11-14 11:36:49 +0900

    feat(Menu): 우테코 식당의 한 메뉴를 표현하는 Menu 도메인 구현
    - 메뉴에 할인을 적용할 수 있는지 결정하고, 할인 금액을 계산하는 책임을 가짐
    - 평일or주말 할인을 인자로 받아서 해당 메뉴에 할인을 적용할 수 있는지 판단, 할인 금액을 계산해서 반환하는 도메인 로직 구현

---
+++ src/domains/Menu.js
@@ -0,0 +1,76 @@
+import {
+  BENEFIT_TYPE,
+  MENU_TYPE,
+  MENU_LIST,
+  DISCOUNT_AMOUNT,
+} from '../constants/christmas-event.js';
+
+export default class Menu {
+  #name;
+
+  #quantity;
+
+  constructor(name, quantity) {
+    this.#name = name;
+    this.#quantity = quantity;
+  }
+
+  #findMenu() {
+    const menu = MENU_LIST.find((item) => item.name === this.#name);
+    return menu;
+  }
+
+  #getMenuType() {
+    const menu = this.#findMenu();
+    return menu.type;
+  }
+
+  #getMenuPrice() {
+    const menu = this.#findMenu();
+    return menu.price;
+  }
+
+  #checkDiscountableMenu(menuType) {
+    return menuType === MENU_TYPE.main || menuType === MENU_TYPE.deesert;
+  }
+
+  #checkDiscountApplicable(menuType, discountType) {
+    if (menuType === MENU_TYPE.main && discountType === BENEFIT_TYPE.weekendDiscount) {
+      return true;
+    }
+
+    if (menuType === MENU_TYPE.deesert && discountType === BENEFIT_TYPE.weekdayDiscount) {
+      return true;
+    }
+
+    return false;
+  }
+
+  #isDiscountable(menuType, discountType) {
+    return (
+      this.#checkDiscountableMenu(menuType) && this.#checkDiscountApplicable(menuType, discountType)
+    );
+  }
+
+  calculateDiscountAmount(discountType) {
+    const menuType = this.#getMenuType();
+    if (!this.#isDiscountable(menuType, discountType)) {
+      return 0;
+    }
+
+    const discountAmount = DISCOUNT_AMOUNT[discountType] * this.#quantity;
+    return discountAmount;
+  }
+
+  calculateMenuAmount() {
+    const menuPrice = this.#getMenuPrice();
+    return menuPrice * this.#quantity;
+  }
+
+  getMenuInfo() {
+    return {
+      name: this.#name,
+      quantity: this.#quantity,
+    };
+  }
+}
